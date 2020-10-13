51f2745bedfdb44ef5ad76069b86cefb774a3dfb
Roney <weiyucheng@sinosoft.com.cn>
2020-10-13 09:56:03 +0800

    feature/support-oracle：增加portal里面的app和app_namespace表

---
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/entity/po/App.java
@@ -0,0 +1,155 @@
+package com.ctrip.framework.apollo.portal.entity.po;
+
+import com.ctrip.framework.apollo.common.entity.BaseEntity;
+import com.ctrip.framework.apollo.common.utils.InputValidator;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "PORTAL_APP",indexes = {
+        @Index(name = "IDX_POAPP_APP_ID", columnList = "APP_ID"),
+        @Index(name = "IDX_POAPP_NAME", columnList = "NAME"),
+        @Index(name = "IDX_POAPP_DATACHANGE_LASTTIME", columnList = "DATACHANGE_LASTTIME"),
+})
+@SQLDelete(sql = "Update PORTAL_APP set DELETED_FLAG = 1 where ID = ?")
+@Where(clause = "DELETED_FLAG = 0")
+public class App extends BaseEntity {
+
+  @NotBlank(message = "Name cannot be blank")
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @NotBlank(message = "AppId cannot be blank")
+  @Pattern(
+      regexp = InputValidator.CLUSTER_NAMESPACE_VALIDATOR,
+      message = InputValidator.INVALID_CLUSTER_NAMESPACE_MESSAGE
+  )
+  @Column(name = "APP_ID", nullable = false)
+  private String appId;
+
+  @Column(name = "ORG_ID", nullable = false)
+  private String orgId;
+
+  @Column(name = "ORG_NAME", nullable = false)
+  private String orgName;
+
+  @NotBlank(message = "OwnerName cannot be blank")
+  @Column(name = "OWNER_NAME", nullable = false)
+  private String ownerName;
+
+  @NotBlank(message = "OwnerEmail cannot be blank")
+  @Column(name = "OWNER_EMAIL", nullable = false)
+  private String ownerEmail;
+
+  public String getAppId() {
+    return appId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getOrgId() {
+    return orgId;
+  }
+
+  public String getOrgName() {
+    return orgName;
+  }
+
+  public String getOwnerEmail() {
+    return ownerEmail;
+  }
+
+  public String getOwnerName() {
+    return ownerName;
+  }
+
+  public void setAppId(String appId) {
+    this.appId = appId;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public void setOrgId(String orgId) {
+    this.orgId = orgId;
+  }
+
+  public void setOrgName(String orgName) {
+    this.orgName = orgName;
+  }
+
+  public void setOwnerEmail(String ownerEmail) {
+    this.ownerEmail = ownerEmail;
+  }
+
+  public void setOwnerName(String ownerName) {
+    this.ownerName = ownerName;
+  }
+
+  public String toString() {
+    return toStringHelper().add("name", name).add("appId", appId)
+        .add("orgId", orgId)
+        .add("orgName", orgName)
+        .add("ownerName", ownerName)
+        .add("ownerEmail", ownerEmail).toString();
+  }
+
+  public static class Builder {
+
+    public Builder() {
+    }
+
+    private App app = new App();
+
+    public Builder name(String name) {
+      app.setName(name);
+      return this;
+    }
+
+    public Builder appId(String appId) {
+      app.setAppId(appId);
+      return this;
+    }
+
+    public Builder orgId(String orgId) {
+      app.setOrgId(orgId);
+      return this;
+    }
+
+    public Builder orgName(String orgName) {
+      app.setOrgName(orgName);
+      return this;
+    }
+
+    public Builder ownerName(String ownerName) {
+      app.setOwnerName(ownerName);
+      return this;
+    }
+
+    public Builder ownerEmail(String ownerEmail) {
+      app.setOwnerEmail(ownerEmail);
+      return this;
+    }
+
+    public App build() {
+      return app;
+    }
+
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+
+}
---
+++ apollo-portal/src/main/java/com/ctrip/framework/apollo/portal/entity/po/AppNamespace.java
@@ -0,0 +1,96 @@
+package com.ctrip.framework.apollo.portal.entity.po;
+
+
+import com.ctrip.framework.apollo.common.entity.BaseEntity;
+import com.ctrip.framework.apollo.common.utils.InputValidator;
+import com.ctrip.framework.apollo.core.enums.ConfigFileFormat;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+@Entity
+@Table(name = "PORTAL_APP_NAMESPACE",indexes = {
+        @Index(name = "IDX_POAPP_NAMESPACE_APP_ID", columnList = "APP_ID"),
+        @Index(name = "IDX_POAPP_NAMESPACE_NAME", columnList = "NAME"),
+        @Index(name = "IDX_POAPPNASP_CHANGE_LASTTIME", columnList = "DATACHANGE_LASTTIME"),
+})
+@SQLDelete(sql = "Update PORTAL_APP_NAMESPACE set DELETED_FLAG = 1 where ID = ?")
+@Where(clause = "DELETED_FLAG = 0")
+public class AppNamespace extends BaseEntity {
+
+  @NotBlank(message = "AppNamespace Name cannot be blank")
+  @Pattern(
+      regexp = InputValidator.CLUSTER_NAMESPACE_VALIDATOR,
+      message = "Invalid Namespace format: " + InputValidator.INVALID_CLUSTER_NAMESPACE_MESSAGE + " & " + InputValidator.INVALID_NAMESPACE_NAMESPACE_MESSAGE
+  )
+  @Column(name = "NAME", nullable = false)
+  private String name;
+
+  @NotBlank(message = "AppId cannot be blank")
+  @Column(name = "APP_ID", nullable = false)
+  private String appId;
+
+  @Column(name = "FORMAT", nullable = false)
+  private String format;
+
+  @Column(name = "PUBLIC_FLAG", columnDefinition = "Number(1) default 0 ")
+  private boolean isPublic = false;
+
+  @Column(name = "COMMENT_MSG")
+  private String comment;
+
+  public String getAppId() {
+    return appId;
+  }
+
+  public String getComment() {
+    return comment;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setAppId(String appId) {
+    this.appId = appId;
+  }
+
+  public void setComment(String comment) {
+    this.comment = comment;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isPublic() {
+    return isPublic;
+  }
+
+  public void setPublic(boolean aPublic) {
+    isPublic = aPublic;
+  }
+
+  public ConfigFileFormat formatAsEnum() {
+    return ConfigFileFormat.fromString(this.format);
+  }
+
+  public String getFormat() {
+    return format;
+  }
+
+  public void setFormat(String format) {
+    this.format = format;
+  }
+
+  public String toString() {
+    return toStringHelper().add("name", name).add("appId", appId).add("comment", comment)
+        .add("format", format).add("isPublic", isPublic).toString();
+  }
+}
